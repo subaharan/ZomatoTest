dcf136e315ee360c17578186810745c51b72750e
Subaharan <subahhran@gmail.com>
2020-10-21 05:40:24 +0530

    alignment changes

--- lib/pages/restaurant_detail.dart
+++ lib/pages/restaurant_detail.dart
@@ -49,205 +49,15 @@ class _RestaurantDetailStatefull extends State<RestaurantDetail> {
           SliverFillRemaining(
             child: ListView(
               children: [
-                Row(
-                  children: [
-                    SizedBox(width: Util.px_15 * SizeConfig.heightMultiplier,),
-                    RatingBar(
-                      initialRating: double.parse(
-                          widget.mRestaurant.userRating.aggregateRating.toString()),
-                      minRating: 1,
-                      itemSize: Util.px_20 * SizeConfig.heightMultiplier,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      itemPadding: EdgeInsets.symmetric(horizontal: 2),
-                      itemBuilder: (context, _) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      onRatingUpdate: (rating) {
-                        print(rating);
-                      },
-                    ),
-                    Text(
-                      widget.mRestaurant.userRating.aggregateRating.toString(),
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: Util.px_13 * SizeConfig.textMultiplier,
-                        fontFamily: 'Roboto',
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-                      margin: EdgeInsets.only(right: Util.px_10 * SizeConfig.heightMultiplier),
-                      child:  Text(
-                       widget.mRestaurant.isDeliveringNow ==1?Strings.delivery_available:Strings.delivery_not_available,
-                        style: TextStyle(
-                          color: widget.mRestaurant.isDeliveringNow==1?Colors.green:Colors.red,
-                          fontSize: Util.px_11 * SizeConfig.textMultiplier,
-                          fontFamily: 'Roboto',
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_5 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
-                  child: RichText(
-                    text: TextSpan(
-                        text: "${widget.mRestaurant.allReviewsCount} ",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                          fontFamily: "ProximaNova",
-                          fontWeight: FontWeight.normal,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: Strings.reviews,
-                              style: TextStyle(
-                                  color: AppColor.grey,
-                                  fontSize: Util.px_15 * SizeConfig.textMultiplier,
-                                  fontFamily: "ProximaNova",
-                                  fontWeight: FontWeight.normal)),
-
-                        ]),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
-                  child: RichText(
-                    text: TextSpan(
-                        text: Strings.address,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                          fontFamily: "ProximaNova",
-                          fontWeight: FontWeight.normal,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: ' : \n${widget.mRestaurant.location.address}, ${widget.mRestaurant.location.localityVerbose}',
-                              style: TextStyle(
-                                  color: AppColor.grey,
-                                  fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                                  fontFamily: "ProximaNova",
-                                  fontWeight: FontWeight.normal)),
-
-                        ]),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
-                  child: RichText(
-                    text: TextSpan(
-                        text: Strings.timings,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                          fontFamily: "ProximaNova",
-                          fontWeight: FontWeight.normal,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: ' : \n${widget.mRestaurant.timings} ',
-                              style: TextStyle(
-                                  color: AppColor.grey,
-                                  fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                                  fontFamily: "ProximaNova",
-                                  fontWeight: FontWeight.normal)),
-
-                        ]),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
-                  child: RichText(
-                    text: TextSpan(
-                        text: Strings.phone_number,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                          fontFamily: "ProximaNova",
-                          fontWeight: FontWeight.normal,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: ' : ${widget.mRestaurant.phoneNumbers} ',
-                              style: TextStyle(
-                                  color: AppColor.grey,
-                                  fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                                  fontFamily: "ProximaNova",
-                                  fontWeight: FontWeight.normal)),
-
-                        ]),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_20 * SizeConfig.heightMultiplier),
-                  alignment: Alignment.center,
-                  height: Util.px_200 * SizeConfig.heightMultiplier,
-                  child: Stack(
-                    children: [
-                      Container(
-
-                        child: Image.network(
-                          widget.mRestaurant.featuredImage,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
-                  child: RichText(
-                    text: TextSpan(
-                        text: Strings.cuisines,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                          fontFamily: "ProximaNova",
-                          fontWeight: FontWeight.normal,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: ' : ${widget.mRestaurant.cuisines} ',
-                              style: TextStyle(
-                                  color: AppColor.grey,
-                                  fontSize: Util.px_14 * SizeConfig.textMultiplier,
-                                  fontFamily: "ProximaNova",
-                                  fontWeight: FontWeight.normal)),
-
-                        ]),
-                  ),
-                ),
+                _ratingWidget(),
+                _reviewWidget(),
+                _addressWidget(),
+                _timingWidget(),
+                _phoneWidget(),
+                _imageWidget(),
+                _cuisinesWidget(),
               ],
-            ) /*WebView(
-                initialUrl: widget.mRestaurant.menuUrl,
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController webViewController) {
-                  _controller.complete(webViewController);
-                },
-
-                navigationDelegate: (NavigationRequest request) {
-                  if (request.url.startsWith('https://www.youtube.com/')) {
-                    print('blocking navigation to $request}');
-                    return NavigationDecision.prevent;
-                  }
-                  print('allowing navigation to $request');
-                  return NavigationDecision.navigate;
-                },
-                onPageStarted: (String url) {
-                  print('Page started loading: $url');
-                },
-                onPageFinished: (String url) {
-                  print('Page finished loading: $url');
-                },
-                gestureNavigationEnabled: false,
-              )*/
-            ,
+            ),
           )
         ],
       ),
@@ -255,6 +65,208 @@ class _RestaurantDetailStatefull extends State<RestaurantDetail> {
   }
 
 
+//-------------
+  Widget _ratingWidget(){
+    return Row(
+      children: [
+        SizedBox(width: Util.px_15 * SizeConfig.heightMultiplier,),
+        RatingBar(
+          initialRating: double.parse(
+              widget.mRestaurant.userRating.aggregateRating.toString()),
+          minRating: 1,
+          itemSize: Util.px_20 * SizeConfig.heightMultiplier,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemPadding: EdgeInsets.symmetric(horizontal: 2),
+          itemBuilder: (context, _) => Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (rating) {
+            print(rating);
+          },
+        ),
+        Text(
+          widget.mRestaurant.userRating.aggregateRating.toString(),
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: Util.px_13 * SizeConfig.textMultiplier,
+            fontFamily: 'Roboto',
+          ),
+        ),
+        Spacer(),
+        Container(
+          margin: EdgeInsets.only(right: Util.px_10 * SizeConfig.heightMultiplier),
+          child:  Text(
+            widget.mRestaurant.isDeliveringNow ==1?Strings.delivery_available:Strings.delivery_not_available,
+            style: TextStyle(
+              color: widget.mRestaurant.isDeliveringNow==1?Colors.green:Colors.red,
+              fontSize: Util.px_11 * SizeConfig.textMultiplier,
+              fontFamily: 'Roboto',
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+//-------------
+  Widget _reviewWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_5 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
+      child: RichText(
+        text: TextSpan(
+            text: "${widget.mRestaurant.allReviewsCount} ",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: Util.px_14 * SizeConfig.textMultiplier,
+              fontFamily: "ProximaNova",
+              fontWeight: FontWeight.normal,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: Strings.reviews,
+                  style: TextStyle(
+                      color: AppColor.grey,
+                      fontSize: Util.px_15 * SizeConfig.textMultiplier,
+                      fontFamily: "ProximaNova",
+                      fontWeight: FontWeight.normal)),
+
+            ]),
+      ),
+    );
+  }
+
+//-------------
+  Widget _addressWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
+      child: RichText(
+        text: TextSpan(
+            text: Strings.address,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: Util.px_14 * SizeConfig.textMultiplier,
+              fontFamily: "ProximaNova",
+              fontWeight: FontWeight.normal,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: ' : \n${widget.mRestaurant.location.address}, ${widget.mRestaurant.location.localityVerbose}',
+                  style: TextStyle(
+                      color: AppColor.grey,
+                      fontSize: Util.px_14 * SizeConfig.textMultiplier,
+                      fontFamily: "ProximaNova",
+                      fontWeight: FontWeight.normal)),
+
+            ]),
+      ),
+    );
+  }
+
+//-------------
+  Widget _timingWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
+      child: RichText(
+        text: TextSpan(
+            text: Strings.timings,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: Util.px_14 * SizeConfig.textMultiplier,
+              fontFamily: "ProximaNova",
+              fontWeight: FontWeight.normal,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: ' : \n${widget.mRestaurant.timings} ',
+                  style: TextStyle(
+                      color: AppColor.grey,
+                      fontSize: Util.px_14 * SizeConfig.textMultiplier,
+                      fontFamily: "ProximaNova",
+                      fontWeight: FontWeight.normal)),
+
+            ]),
+      ),
+    );
+  }
+
+//-------------
+  Widget _phoneWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
+      child: RichText(
+        text: TextSpan(
+            text: Strings.phone_number,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: Util.px_14 * SizeConfig.textMultiplier,
+              fontFamily: "ProximaNova",
+              fontWeight: FontWeight.normal,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: ' : ${widget.mRestaurant.phoneNumbers} ',
+                  style: TextStyle(
+                      color: AppColor.grey,
+                      fontSize: Util.px_14 * SizeConfig.textMultiplier,
+                      fontFamily: "ProximaNova",
+                      fontWeight: FontWeight.normal)),
+
+            ]),
+      ),
+    );
+  }
+
+//-------------
+  Widget _cuisinesWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_10 * SizeConfig.heightMultiplier, left: Util.px_15 * SizeConfig.heightMultiplier),
+      child: RichText(
+        text: TextSpan(
+            text: Strings.cuisines,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: Util.px_14 * SizeConfig.textMultiplier,
+              fontFamily: "ProximaNova",
+              fontWeight: FontWeight.normal,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                  text: ' : ${widget.mRestaurant.cuisines} ',
+                  style: TextStyle(
+                      color: AppColor.grey,
+                      fontSize: Util.px_14 * SizeConfig.textMultiplier,
+                      fontFamily: "ProximaNova",
+                      fontWeight: FontWeight.normal)),
+
+            ]),
+      ),
+    );
+  }
+
+//-------------
+Widget _imageWidget(){
+    return Container(
+      margin: EdgeInsets.only(top: Util.px_20 * SizeConfig.heightMultiplier),
+      alignment: Alignment.center,
+      height: Util.px_200 * SizeConfig.heightMultiplier,
+      child: Stack(
+        children: [
+          Container(
+
+            child: Image.network(
+              widget.mRestaurant.featuredImage,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
+}
+
+//-------------
   Future<void> _launchInWebViewWithJavaScript(String url) async {
     if (await canLaunch(url)) {
       await launch(
